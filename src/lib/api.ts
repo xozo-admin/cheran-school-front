7f4efb30f92a88dc0cda8f3005a58baaec6c7c47
xozo-devops <admin@xozotech.in>
2026-06-23 13:46:44 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,8 +10,8 @@ const ENABLE_ENCRYPTION =
   process.env.NEXT_PUBLIC_ENABLE_ENCRYPTION?.toLowerCase() === 'true';
 
 
-export const backend_api = 'http://10.153.15.71:8000';
-const API_BASE_HTTP = 'http://10.153.15.71:8000/api/';
+export const backend_api = 'https://macid.pythonanywhere.com';
+const API_BASE_HTTP = 'https://macid.pythonanywhere.com/api/';
 
 
 declare module 'axios' {
